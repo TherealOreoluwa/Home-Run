802baa13b9a6e11264a0ab20ae42375aea527215
TherealOreoluwa <oreoluwaadesina82@gmail.com>
2024-05-23 11:56:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// document.getElementById('Demo1').innerHTML=Demo1;
+
+function Increase(){
+    document.getElementById('Demo1').style.fontSize = '55px';
+}
+ 
+document.getElementById('b1').style.textAlign = 'center';
+
+document.getElementById('b1a').style.textAlign = 'center';
+document.getElementById('b1b').style.textAlign = 'center';
+
+document.getElementById('c1').style.textAlign = 'center';
+document.getElementById('c1a').style.textAlign = 'center';
+document.getElementById('c1b').style.textAlign = 'center';
+
